0736e2f74cd8afab96cb0535a495b927b8d882bf
Tess Grankina <taisa.grankina@gmail.com>
2022-07-22 21:47:25 +0200

    remove gitignore files

--- src/sections/Header.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from '../components/pdText';
-
-export const Header = () => {
-    return (
-        <div>
-            <Text logoText alignLeft>The Planday Challange</Text>
-        </div>
-    );
-}
--- src/utils/types.tsx
+++
@@ -1,9 +0,0 @@
-export interface PDCard {
-    title: string;
-    description: string;
-    imagePath: string;
-};
-
-export interface PDCards {
-    cards: PDCard[];
-}
